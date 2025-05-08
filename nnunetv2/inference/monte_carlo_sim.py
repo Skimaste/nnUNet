86af95d6e5a9629a65b84b0196a23d7549e3b4d1
Skimaste <emil.juul.elberg@gmail.com>
2025-05-08 10:17:04 +0200

    mc bugfix

--- nnunetv2/inference/monte_carlo_sim.py
+++ nnunetv2/inference/monte_carlo_sim.py
@@ -9,103 +9,91 @@ import shutil
 import nibabel as nib
 
  # nnUNetv2_predict -d 3 -f 0 -c 3d_lowres -i imagesTs -o imagesTs_predlowres --continue_prediction
+if __name__ == "__main__":
+    dropout_p = '02' # prob in 10%
+    dropout_s = '2'
+    n_sim = 3
+    cuda_device = 2
+    n_cases = 1
 
-dropout_p = '02' # prob in 10%
-dropout_s = '2'
-n_sim = 3
-cuda_device = 2
-n_cases = 1
+    folder = join(nnUNet_raw, 'Dataset003_ImageCAS_split/imagesTs')
 
-folder = join(nnUNet_raw, 'Dataset003_ImageCAS_split/imagesTs')
+    cases = [os.path.splitext(f)[0].split('_')[1] for f in sorted(os.listdir(folder)) if os.path.isfile(join(folder, f))][:n_cases]
 
-cases = [os.path.splitext(f)[0].split('_')[1] for f in sorted(os.listdir(folder)) if os.path.isfile(join(folder, f))][:n_cases]
-
-# cases = [join(folder, f) for f in sorted(os.listdir(folder)) if os.path.isfile(join(folder, f))][:n_cases] # ['0002', '0015']
-
-print(cases)
-
-
- # instantiate the nnUNetPredictorMCDropout
-predictor = nnUNetPredictorMCDropout(
-    tile_step_size=0.5,
-    use_gaussian=True,
-    use_mirroring=True,
-    perform_everything_on_device=True,
-    device=torch.device('cuda', cuda_device),
-    verbose=False,
-    verbose_preprocessing=False,
-    allow_tqdm=True
-)
-# initializes the network architecture, loads the checkpoint
-predictor.initialize_from_trained_model_folder(
-    join(nnUNet_results, f'Dataset003_ImageCAS_split/nnUNetTrainerDropout__p{dropout_p}_s{dropout_s}__3d_fullres'),
-    use_folds=(0,),
-    checkpoint_name='checkpoint_final.pth',
-)
+    # instantiate the nnUNetPredictorMCDropout
+    predictor = nnUNetPredictorMCDropout(
+        tile_step_size=0.5,
+        use_gaussian=True,
+        use_mirroring=True,
+        perform_everything_on_device=True,
+        device=torch.device('cuda', cuda_device),
+        verbose=False,
+        verbose_preprocessing=False,
+        allow_tqdm=True
+    )
+    # initializes the network architecture, loads the checkpoint
+    predictor.initialize_from_trained_model_folder(
+        join(nnUNet_results, f'Dataset003_ImageCAS_split/nnUNetTrainerDropout__p{dropout_p}_s{dropout_s}__3d_fullres'),
+        use_folds=(0,),
+        checkpoint_name='checkpoint_final.pth',
+    )
 
 
-for case in cases:
-    indir = [join(folder, f'case_{case}_0000.nii.gz')]
-    indirs = [indir for _ in range(n_sim)]
+    for case in cases:
+        indir = [join(folder, f'case_{case}_0000.nii.gz')]
+        indirs = [indir for _ in range(n_sim)]
 
-    #outdir = [join(nnUNet_raw, f'Dataset003_ImageCAS_split/imagesTs/case_{case}/case_{case}_sim_{n}.nii.gz')]
-    temp_outdir = [join(nnUNet_results, f'Dataset003_ImageCAS_split/temp')]
-    temp_outdirs = [join(temp_outdir, f'sim_{i}.nii.gz' for i in range(n_sim))]
+        #outdir = [join(nnUNet_raw, f'Dataset003_ImageCAS_split/imagesTs/case_{case}/case_{case}_sim_{n}.nii.gz')]
+        temp_outdir = join(nnUNet_results, f'Dataset003_ImageCAS_split/temp')
+        temp_outdirs = [join(temp_outdir, f'sim_{i}.nii.gz') for i in range(n_sim)]
 
-    outdir = join(nnUNet_results, f'Dataset003_ImageCAS_split/case_{case}')
+        outdir = join(nnUNet_results, f'Dataset003_ImageCAS_split/case_{case}')
 
-    predictor.predict_from_files(
-        indirs,
-        temp_outdirs,
-        save_probabilities=True,
-        overwrite=True,
-        num_processes_preprocessing=2,
-        num_processes_segmentation_export=2,
-        folder_with_segs_from_prev_stage=None,
-        num_parts=1,
-        part_id=0)
+        predictor.predict_from_files(
+            indirs,
+            temp_outdirs,
+            save_probabilities=True,
+            overwrite=True,
+            num_processes_preprocessing=2,
+            num_processes_segmentation_export=2,
+            folder_with_segs_from_prev_stage=None,
+            num_parts=1,
+            part_id=0)
 
-    # npz_files = [join(nnUNet_raw, f'Dataset003_ImageCAS_split/imagesTs_predfullres/case_{case}_sim_{n}.npz') for n in range(nsim)]
-    npz_files = [join(temp_outdir, f'sim_{i}.npz' for i in range(n_sim))]
+        # npz_files = [join(nnUNet_raw, f'Dataset003_ImageCAS_split/imagesTs_predfullres/case_{case}_sim_{n}.npz') for n in range(nsim)]
+        npz_files = [join(temp_outdir, f'sim_{i}.npz') for i in range(n_sim)]
 
-     # Load data from each file
-    loaded_data = [np.load(f) for f in npz_files]
+        # Load data from each file
+        loaded_data = [np.load(f) for f in npz_files]
 
-     # Assuming all files have the same keys
-    keys = loaded_data[0].files  # e.g., ['arr_0']
+        # Assuming all files have the same keys
+        keys = loaded_data[0].files  # e.g., ['arr_0']
 
-     # Concatenate along a new dimension (e.g., axis=0)
-    concatenated_data = {
-        key: np.stack([data[key].astype(np.float32) for data in loaded_data], axis=0)
-        for key in keys
-    }
+        # Concatenate along a new dimension (e.g., axis=0)
+        concatenated_data = {
+            key: np.stack([data[key].astype(np.float32) for data in loaded_data], axis=0)
+            for key in keys
+        }
 
-     # Save the concatenated result to a new file
-    np.savez_compressed(join(outdir, f'case_{case}_merged.npz'), **concatenated_data, stacked=stacked)
+        # Save the concatenated result to a new file
+        np.savez_compressed(join(outdir, f'case_{case}_merged.npz'), **concatenated_data)
+        
+        shutil.rmtree(temp_outdir)
 
-     # Optional: remove original files
-    # for f in npz_files:
-        # os.remove(f)
-    shutil.rmtree(temp_outdir)
+        # calc var-uncertainty and mean
 
+        data = np.load(join(outdir, f'case_{case}_merged.npz'))
 
+        data = data[data.files[0]][:, 1, :, :, :]
 
-    # calc var-uncertainty and mean
+        data_var = np.var(data, axis = 0)
+        data_mean = np.mean(data, axis = 0)
 
-    data = np.load(join(outdir, f'case_{case}_merged.npz'))
+        affine = nib.load(indir[0]).affine
 
-    data = data[data.files[0]][:, 1, :, :, :]
-
-    data_var = np.var(data, axis = 0)
-    data_mean = np.mean(data, axis = 0)
-
-    affine = nib.load(indir*).affine
-
-    nib.Nifti1Image(data_var, affine).to_filename(join(outdir, f'case_{case}_var.nii.gz'))
-    nib.Nifti1Image(data_mean, affine).to_filename(join(outdir, f'case_{case}_mean.nii.gz'))
+        nib.Nifti1Image(data_var, affine).to_filename(join(outdir, f'case_{case}_var.nii.gz'))
+        nib.Nifti1Image(data_mean, affine).to_filename(join(outdir, f'case_{case}_mean.nii.gz'))
 
 
-
-    
 
 
