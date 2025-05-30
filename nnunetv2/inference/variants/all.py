0a1529083dabdf1d2af1d913356f9b73dbb211ef
Skimaste <emil.juul.elberg@gmail.com>
2025-05-30 13:41:40 +0200

    prepare for inference of no mcd

--- nnunetv2/inference/variants/all.py
+++ nnunetv2/inference/variants/all.py
@@ -5,15 +5,55 @@ if __name__ == '__main__':
 
     n_cases = 20 # maybe 100 for final results??
 
-    all = MonteCarloInference( # done
+    base = MonteCarloInference( # done
         dataset_name=dataset_name,
-        model='nnUNetTrainerDropout__p02_s2__3d_fullres',
+        model='nnUNetTrainerDropout__p00_s2__3d_fullres',
         n_cases=n_cases,
-        n_sim=30,
-        folds=(0, 1, 2, 3, 4),
+        n_sim=1,
+        folds=(0,),
         cuda_device=3,
-        config_name='all',
+        config_name='base',
+        tta=False
+    )
+
+    base.run()
+
+    tta = MonteCarloInference( # done
+        dataset_name=dataset_name,
+        model='nnUNetTrainerDropout__p00_s2__3d_fullres',
+        n_cases=n_cases,
+        n_sim=1,
+        folds=(0,),
+        cuda_device=3,
+        config_name='tta',
         tta=True
     )
 
-    all.run()
+    tta.run()
+
+    ens = MonteCarloInference( # done
+        dataset_name=dataset_name,
+        model='nnUNetTrainerDropout__p00_s2__3d_fullres',
+        n_cases=n_cases,
+        n_sim=1,
+        folds=(0, 1, 2, 3, 4),
+        cuda_device=3,
+        config_name='ens',
+        tta=False
+    )
+
+    ens.run()
+
+    tta_ens = MonteCarloInference( # done
+        dataset_name=dataset_name,
+        model='nnUNetTrainerDropout__p00_s2__3d_fullres',
+        n_cases=n_cases,
+        n_sim=1,
+        folds=(0, 1, 2, 3, 4),
+        cuda_device=3,
+        config_name='tta_ens',
+        tta=True
+    )
+
+    tta_ens.run()
+
